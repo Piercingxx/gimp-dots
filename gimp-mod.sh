b82d2911eb300e818018a0480edc7139c82db3c6
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-06-27 10:46:49 -0400

    update

--- gimp-mod.sh
+++ gimp-mod.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 # GitHub.com/PiercingXX
 
-                    sudo rm -Rf /home/"$username"/.var/app/org.gimp.GIMP/config/GIMP/*
-                    sudo rm -Rf /home/"$username"/.config/GIMP/*
-                    mkdir -p /home/"$username"/.config/GIMP/2.1
-                    chown -R "$username":"$username" /home/"$username"/.config/GIMP
-                    cd gimp-dots/Gimp || exit
-                    cp -Rf 3.0/* /home/"$username"/.config/GIMP/2.1
-                    chown "$username":"$username" -R /home/"$username"/.config/GIMP
-
+                # GIMP 3.0
                     sudo rm -Rf /home/"$username"/.var/app/org.gimp.GIMP/config/GIMP/*
                     sudo rm -Rf /home/"$username"/.config/GIMP/*
                     mkdir -p /home/"$username"/.config/GIMP/3.0
@@ -16,7 +9,7 @@
                     cd gimp-dots/Gimp || exit
                     cp -Rf 3.0/* /home/"$username"/.config/GIMP/3.0
                     chown "$username":"$username" -R /home/"$username"/.config/GIMP
-
+                # GIMP 3.1
                     sudo rm -Rf /home/"$username"/.var/app/org.gimp.GIMP/config/GIMP/*
                     sudo rm -Rf /home/"$username"/.config/GIMP/*
                     mkdir -p /home/"$username"/.config/GIMP/3.1
