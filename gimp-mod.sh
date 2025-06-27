a03c90088883d3b6a12fa25b479b0987c29c20d8
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-06-27 06:41:22 -0400

    Create gimp-mod.sh

---
+++ gimp-mod.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# GitHub.com/PiercingXX
+
+                    sudo rm -Rf /home/"$username"/.var/app/org.gimp.GIMP/config/GIMP/*
+                    sudo rm -Rf /home/"$username"/.config/GIMP/*
+                    mkdir -p /home/"$username"/.config/GIMP/2.1
+                    chown -R "$username":"$username" /home/"$username"/.config/GIMP
+                    cd gimp-dots/Gimp || exit
+                    cp -Rf 3.0/* /home/"$username"/.config/GIMP/2.1
+                    chown "$username":"$username" -R /home/"$username"/.config/GIMP
+
+                    sudo rm -Rf /home/"$username"/.var/app/org.gimp.GIMP/config/GIMP/*
+                    sudo rm -Rf /home/"$username"/.config/GIMP/*
+                    mkdir -p /home/"$username"/.config/GIMP/3.0
+                    chown -R "$username":"$username" /home/"$username"/.config/GIMP
+                    cd gimp-dots/Gimp || exit
+                    cp -Rf 3.0/* /home/"$username"/.config/GIMP/3.0
+                    chown "$username":"$username" -R /home/"$username"/.config/GIMP
+
+                    sudo rm -Rf /home/"$username"/.var/app/org.gimp.GIMP/config/GIMP/*
+                    sudo rm -Rf /home/"$username"/.config/GIMP/*
+                    mkdir -p /home/"$username"/.config/GIMP/3.1
+                    chown -R "$username":"$username" /home/"$username"/.config/GIMP
+                    cd gimp-dots/Gimp || exit
+                    cp -Rf 3.0/* /home/"$username"/.config/GIMP/3.1
+                    chown "$username":"$username" -R /home/"$username"/.config/GIMP
